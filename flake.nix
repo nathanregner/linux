a49dbb69a8efca90e6beaeecf6ffbfae5fadaf1c
Nathan Regner <nathanregner@gmail.com>
2024-12-27 17:36:37 -0700

    add direnv

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    {
+      devShells.x86_64-linux.default =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        pkgs.mkShell {
+          packages = [
+            pkgs.gnumake
+            pkgs.clang
+          ];
+        };
+    };
+}

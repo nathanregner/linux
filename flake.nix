7fc3fd7c0751ba2e191029d25983e7d998d37548
Nathan Regner <nathanregner@gmail.com>
2025-01-11 10:04:19 -0700

    fixup! add direnv

--- flake.nix
+++ flake.nix
@@ -1,6 +1,4 @@
 {
-  description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
@@ -13,10 +11,29 @@
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
         in
         pkgs.mkShell {
-          packages = [
-            pkgs.gnumake
-            pkgs.clang
-          ];
+          env = {
+            ARCH = "arm64";
+            CROSS_COMPILE = "aarch64-unknown-linux-gnu-";
+            LLVM = "1";
+            PKG_CONFIG_PATH = "${pkgs.ncurses.dev}/lib/pkgconfig";
+            # KCFLAGS = "-I${pkgs.llvmPackages.clang}/resource-root/include -Wno-everything -march=armv8-a+crypto -Wno-error=unused-command-line-argument";
+          };
+          packages = builtins.attrValues {
+            inherit (pkgs)
+              bc
+              bison
+              flex
+              gnumake
+              ncurses
+              openssl
+              pkg-config
+              ;
+            inherit (pkgs.llvmPackages)
+              bintools-unwrapped
+              clang
+              stdenv
+              ;
+          };
         };
     };
 }

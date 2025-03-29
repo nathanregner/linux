c379a856159b76566ef05c2381dbc31221d94c51
Nathan Regner <nathanregner@gmail.com>
2025-03-28 19:30:48 -0600

    switch back to gcc

--- flake.nix
+++ flake.nix
@@ -9,34 +9,50 @@
       devShells.x86_64-linux.default =
         let
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          # stdenv = pkgs.overrideCC pkgs.clangStdenv (
+          #   pkgs.ccacheWrapper.override { inherit (pkgs.clangStdenv) cc; }
+          # );
+          # stdenv = pkgs.pkgsCross.aarch64-multiplatform.ccacheStdenv;
+          # stdenv = pkgs.ccacheStdenv;
+          # inherit (pkgs) stdenv;
+          # stdenv = (pkgs.pkgsCross.aarch64-multiplatform) stdenv;
+          inherit (pkgs.pkgsCross.aarch64-multiplatform) stdenv;
         in
-        (pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }) {
-          env = {
-            ARCH = "arm64";
-            CROSS_COMPILE = "aarch64-unknown-linux-gnu-";
-            LLVM = "1";
-            PKG_CONFIG_PATH = "${pkgs.ncurses.dev}/lib/pkgconfig";
-            # KCFLAGS = "-I${pkgs.llvmPackages.clang}/resource-root/include -Wno-everything -march=armv8-a+crypto -Wno-error=unused-command-line-argument";
+        (pkgs.mkShell.override {
+          inherit stdenv;
+        })
+          {
+            env = {
+              # ARCH = "arm64";
+              # CROSS_COMPILE = "aarch64-unknown-linux-gnu-";
+              # LLVM = "1";
+              # PKG_CONFIG_PATH = "${pkgs.ncurses.dev}/lib/pkgconfig";
+              # KCFLAGS = "-I${pkgs.llvmPackages.clang}/resource-root/include -Wno-everything -march=armv8-a+crypto -Wno-error=unused-command-line-argument";
+              # KCFLAGS = "-Wno-everything -Wno-error=unused-command-line-argument";
+              ARCH = stdenv.hostPlatform.linuxArch;
+              CROSS_COMPILE = stdenv.cc.targetPrefix;
+            };
+            shellHook = ''
+              alias make="make -j$NIX_BUILD_CORES"
+            '';
+            packages = builtins.attrValues {
+              inherit (pkgs)
+                bc
+                bison
+                flex
+                ncurses
+                openssl
+                pkg-config
+                stdenv
+                ;
+              inherit (pkgs.stdenv) cc;
+              # inherit (pkgs.pkgsCross.aarch64-multiplatform) stdenv;
+              # inherit (pkgs.llvmPackages)
+              #   bintools-unwrapped
+              #   clang
+              #   stdenv
+              #   ;
+            };
           };
-          shellHook = ''
-            alias make="make -j$NIX_BUILD_CORES"
-          '';
-          packages = builtins.attrValues {
-            inherit (pkgs)
-              bc
-              bison
-              flex
-              # gnumake
-              # ncurses
-              openssl
-              pkg-config
-              ;
-            inherit (pkgs.llvmPackages)
-              bintools-unwrapped
-              clang
-              stdenv
-              ;
-          };
-        };
     };
 }

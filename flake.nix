6dca64609f3efc27f807e2ccb55805ca3170b481
Nathan Regner <nathanregner@gmail.com>
2025-10-14 22:10:31 -0600

    fixup! fixup! switch back to gcc

--- flake.nix
+++ flake.nix
@@ -10,56 +10,31 @@
         let
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
           inherit (pkgs) lib;
-          # stdenv = pkgs.overrideCC pkgs.clangStdenv (
-          #   pkgs.ccacheWrapper.override { inherit (pkgs.clangStdenv) cc; }
-          # );
-          # stdenv = pkgs.pkgsCross.aarch64-multiplatform.ccacheStdenv;
-          # stdenv = pkgs.ccacheStdenv;
-          # inherit (pkgs) stdenv;
-          # stdenv = (pkgs.pkgsCross.aarch64-multiplatform) stdenv;
-          inherit (pkgs.pkgsCross.aarch64-multiplatform) stdenv;
+          pkgsCross = pkgs.pkgsCross.aarch64-multiplatform;
         in
-        (pkgs.mkShell.override {
-          inherit stdenv;
-        })
-          {
-            env = {
-              # ARCH = "arm64";
-              # CROSS_COMPILE = "aarch64-unknown-linux-gnu-";
-              # LLVM = "1";
-              PKG_CONFIG_PATH = lib.concatStringsSep ";" [
-                "${pkgs.ncurses}/lib/pkgconfig"
-                "${pkgs.ncurses.dev}/lib/pkgconfig"
-                "${pkgs.openssl.dev}/lib/pkgconfig"
-              ];
-              # KCFLAGS = "-I${pkgs.llvmPackages.clang}/resource-root/include -Wno-everything -march=armv8-a+crypto -Wno-error=unused-command-line-argument";
-              # KCFLAGS = "-Wno-everything -Wno-error=unused-command-line-argument";
-              ARCH = stdenv.hostPlatform.linuxArch;
-              CROSS_COMPILE = stdenv.cc.targetPrefix;
-            };
-            # O=build
-            shellHook = ''
-              alias make="make -j$NIX_BUILD_CORES"
-            '';
-            packages = builtins.attrValues {
-              inherit (pkgs)
-                bc
-                bison
-                flex
-                ncurses
-                openssl
-                pkg-config
-                stdenv
-                ubootTools
-                ;
-              inherit (pkgs.stdenv) cc;
-              # inherit (pkgs.pkgsCross.aarch64-multiplatform) stdenv;
-              # inherit (pkgs.llvmPackages)
-              #   bintools-unwrapped
-              #   clang
-              #   stdenv
-              #   ;
-            };
+        pkgsCross.mkShell {
+          env = {
+            ARCH = pkgsCross.stdenv.hostPlatform.linuxArch;
+            CROSS_COMPILE = pkgsCross.stdenv.cc.targetPrefix;
+            PKG_CONFIG_PATH = lib.concatStringsSep ":" [
+              "${pkgs.ncurses.dev}/lib/pkgconfig"
+              "${pkgs.openssl.dev}/lib/pkgconfig"
+            ];
           };
+          # mkdir build
+          # make O=build nconfig
+          # make O=build -j12
+          shellHook = ''
+            alias make="make -j$NIX_BUILD_CORES"
+          '';
+          packages = [
+            pkgs.bc
+            pkgs.bison
+            pkgs.flex
+            pkgs.pkg-config
+            pkgs.stdenv.cc
+            pkgs.ubootTools
+          ];
+        };
     };
 }
